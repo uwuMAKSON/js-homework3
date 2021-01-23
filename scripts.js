20af641613c911125e6b73824f38272da02bbc8a
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-01-23 14:16:34 +0600

    finish

---
+++ scripts.js
@@ -0,0 +1,117 @@
+let answer1 = prompt("Новый год на разных Языках");
+switch (answer1){
+case "Кыргызстан":
+case "Киргизия":
+console.log("Жаны Жыл!");
+break;
+case "Рассийский":
+case "Rashin":
+console.log("Новый Год!");
+default:
+console.log("Нет");
+break;
+}
+
+let answer2 = prompt("Название Китайя на Разных языках");
+switch (answer2){
+case "Рассия":
+case "America":
+console.log("Chine");
+break;
+case  "Казакстан":
+case  "Кыргызстан":  
+console.log("Кытай");
+default:
+console.log("Нет");
+break;
+}
+
+let answer3 = prompt("Назваеие Школы на разных языках!");
+switch (answer3){
+case "Кыргызстан":
+case "Кыргиском":
+console.log("Мектеп!");
+break;
+case "Рассия":
+case "Rashin":
+console.log("школа!");
+break;
+case "English":
+case "Англиский":
+console.log("School");
+default:
+console.log("Нет");
+break;
+}
+
+let answer4 = prompt("Название каров в регионов Кыргызстане!");
+switch (answer4){
+case "Ош":
+case "Чуй":
+console.log("сыйыр!");
+break;
+case  "Ысык-Кол":
+case  "Нарын":  
+console.log("Уй");
+default:
+console.log("Нет");
+break;
+}
+
+let answer5 = prompt("книга на разных языках!");
+switch (answer5){
+case " Кыргызстан":
+case "Киргизия":
+console.log("Китеп!");
+break;
+case  "Рассия":
+case  "Рассийский":  
+console.log("Книга");
+default:
+console.log("Нет");
+break;
+ }
+
+let unit = prompt("Wath unit?");
+let temperature =prompt("wath temperature?");
+
+
+
+if(unit == "C"){
+if(temperature == 0 || temperature == 273 || temperature == 32){
+console.log("Yes");
+}
+else {console.log("NO")
+}
+}
+ 
+let unit4 = prompt("Кто такой Манас?");
+
+if(unit4 == "Баатыр" || unit4 == "Кыргыздын ханы" || unit4 == "R"){
+console.log("Yes");
+}
+else {console.log("NO")
+}
+ 
+let unit1 = prompt("Тоголок.Молдо ким?");
+if(unit1 == "Акын" || unit1 == "Ревалюцонер" || unit1 == "Комузчу"){
+console.log("Yes");
+}
+else {console.log("NO")
+}
+ 
+
+let unit2 = prompt("Манас канчанчы кылымда болгон");
+
+if( unit2 == 7 ||  unit2 == 8 ||  unit2 == 10){
+console.log("Yes");
+}
+else {console.log("NO")
+}
+
+let unit3 = prompt("Wath unit?");
+if(unit3  == 0 || unit3  == 273 || unit3  == 32){
+console.log("Yes");
+}
+else {console.log("NO")
+}
